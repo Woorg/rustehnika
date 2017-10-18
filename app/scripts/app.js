bb724c9233596dd77294ae3906d87a979246e861
Igor Gorlov <gorlov35@gmail.com>
2017-10-18 23:21:09 +0300

    add index adaptive

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,6 +19,14 @@ import 'slick-carousel';
 			appendDots: '.slider__dots'
 		});
 
+
+		// main product specs
+
+		const $mainProdSpecsLink = $('.products__specs-link');
+
+		$mainProdSpecsLink.on('click', function () {
+			$(this).next().toggleClass('products__specs-list_active');
+		});
 	});
 
 })(jQuery);
