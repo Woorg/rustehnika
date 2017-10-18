d24e6670ec682a3b9e49a59f0709041dc0683f01
Igor Gorlov <gorlov35@gmail.com>
2017-10-18 20:09:04 +0300

    add index page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,25 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import 'slick-carousel';
 
-$(() => {
-	svg4everybody();
-});
+
+(function ($) {
+	$(function () {
+		svg4everybody();
+
+		// Slider
+
+		$('.slider__list').slick({
+			slidesToShow: 1,
+			infinite: true,
+			dots: true,
+			fade: true,
+			cssEase: 'linear',
+			arrows: false,
+			appendDots: '.slider__dots'
+		});
+
+	});
+
+})(jQuery);
+
