5c87635455ab8c5327ac836a125aa269da2cd934
Igor Gorlov <gorlov35@gmail.com>
2017-10-27 03:12:43 +0300

    add cart page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,23 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 import 'slick-carousel';
-
+import 'magnific-popup';
+import 'jquery-ui/ui/core';
+import 'jquery-ui/ui/widget';
+// import 'jquery-ui/ui/effect';
+// import 'jquery-ui/ui/widgets/mouse';
+import 'jquery-ui/ui/widgets/button';
+import 'jquery-ui/ui/widgets/spinner';
 
 (function ($) {
 	$(function () {
 		svg4everybody();
 
-		// Slider
+		/**
+		 *
+		 * Slider
+		 *
+		 */
 
 		$('.slider__list').slick({
 			slidesToShow: 1,
@@ -20,7 +30,11 @@ import 'slick-carousel';
 		});
 
 
-		// main product specs
+		/**
+		 *
+		 * main product specs
+		 *
+		 */
 
 		const $mainProdSpecsLink = $('.products__specs-link');
 
@@ -28,12 +42,49 @@ import 'slick-carousel';
 			$(this).next().toggleClass('products__specs-list_active');
 		});
 
-		// catalog product specs
+		/**
+		 *
+		 * catalog product specs
+		 *
+		 */
 
 		const $catProdLnk = $('.catalog__specs-link');
 		$catProdLnk.on('click', function () {
 			$(this).next().toggleClass('catalog__specs-list_active');
 		});
+
+		/**
+		 *
+		 * Basket quant spinner
+		 *
+		 */
+
+		const quantInput = $('.quant__input');
+		const quantUp = $('.quant__button_up');
+		const quantDown = $('.quant__button_down');
+
+
+		quantInput.click(function() {
+		   return false;
+		})
+
+		quantInput.spinner({
+		   max: 1000,
+		   min: 0,
+		   step: 1
+		});
+
+		quantDown.click(function() {
+		   $(this).parent().find('.quant__input').spinner('stepDown');
+		   return false;
+		});
+		quantUp.click(function() {
+		   $(this).parent().find('.quant__input').spinner('stepUp');
+		   return false;
+		});
+
+
+
 	});
 
 })(jQuery);
